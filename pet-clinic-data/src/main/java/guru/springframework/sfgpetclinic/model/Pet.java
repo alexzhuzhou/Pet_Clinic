4657d230ac3492103ff8d0fa04d89decf59746ec
alexzhuzhou <alexzhuzhou@gmail.com>
2022-04-16 04:41:28 +0200

    Added Visit Controller And Mappings .Closes #56

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -20,7 +20,10 @@ public class Pet extends BaseEntity {
         this.petType = petType;
         this.owner = owner;
         BirthDate = birthDate;
-        this.visits = visits;
+
+        if(visits == null || visits.size() > 0) {
+            this.visits = visits;
+        }
     }
 
     @Column(name = "name")
